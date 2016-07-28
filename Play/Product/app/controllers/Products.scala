55146bcd7d4ebce5404f0bf74934201130e89029
llxmedici <llxmedici@gmail.com>
2016-07-28 22:06:40 +0800

    basic render works

--- Play/Product/app/controllers/Products.scala
+++ Play/Product/app/controllers/Products.scala
@@ -3,13 +3,13 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import models.Product
 
 /**
   * Created by llxmedici on 7/28/16.
   */
-@Singleton
-class Products @Inject() extends Controller {
+class Products @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
   def list = Action { implicit request =>
     val products = Product.findAll
     Ok(views.html.list(products))
